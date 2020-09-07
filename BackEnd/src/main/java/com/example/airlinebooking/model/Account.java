165db84b8b623e9eb9c7f6ef1325b7e7c6eee268
s3820369 <s3820369@student.rmit.edu.au>
2020-09-07 10:59:07 +1000

    Add Account.java

---
+++ BackEnd/src/main/java/com/example/airlinebooking/model/Account.java
@@ -0,0 +1,112 @@
+package com.example.airlinebooking.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String gender;
+    private String country;
+    private String address;
+    private String phoneNum;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+
+
+    public Account() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt= new Date();
+    }
+     @PreUpdate
+    protected  void onUpdate(){
+        this.updatedAt = new Date();
+     }
+
+
+}
