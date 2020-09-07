a8d04e6347b76fed2c3830eb04c94388ca8c8a02
s3820369 <s3820369@student.rmit.edu.au>
2020-09-07 12:37:12 +1000

    Add 3 files

---
+++ BackEnd/src/main/java/com/example/airlinebooking/model/TicketDetails.java
@@ -0,0 +1,75 @@
+package com.example.airlinebooking.model;
+
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TicketDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String PassportNumber;
+    private String fullName;
+    private String flightID;
+    private String departureDate;
+    private String status;
+
+    public TicketDetails(long id, String passportNumber, String fullName, String flightID, String departureDate, String status) {
+        this.id = id;
+        this.PassportNumber = passportNumber;
+        this.fullName = fullName;
+        this.flightID = flightID;
+        this.departureDate = departureDate;
+        this.status = status;
+    }
+
+    public String getPassportNumber() {
+        return PassportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        PassportNumber = passportNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFlightID() {
+        return flightID;
+    }
+
+    public void setFlightID(String flightID) {
+        this.flightID = flightID;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public TicketDetails() {
+
+    }
+}
