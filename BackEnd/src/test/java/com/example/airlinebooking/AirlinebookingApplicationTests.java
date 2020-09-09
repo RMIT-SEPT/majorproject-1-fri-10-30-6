7f998048b5a01025eef5611e6d26090fa325402e
s3820369 <s3820369@student.rmit.edu.au>
2020-09-10 00:14:40 +1000

    add files

---
+++ BackEnd/src/test/java/com/example/airlinebooking/AirlinebookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinebookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
